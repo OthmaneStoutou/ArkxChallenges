c3510783d093acae46bd6ef619a248750aeb9567
othmane <stoutouothmane@gmail.com>
2024-01-10 10:03:42 +0100

    new file

---
+++ Loops.js
@@ -0,0 +1,80 @@
+
+
+//Task 1 Factorial :
+
+let num = 6
+let fact = 1
+
+if (num==0 || num==1){
+    console.log(1)
+}
+else{
+ for(let n=1;n <= num ;n++){
+     fact = fact*n
+ }
+}
+console.log(fact)
+
+
+//Task 2 Length Of a Number :
+
+// let Num = 1234567 
+// let C = 0
+
+// do {
+//     Num =Num/10
+//     C = C++
+// } while (Num >=0);
+
+// console(C)
+
+// while(Num!==0){
+//     Num = Num / 10 ;
+//     C++ ;
+// }
+// console.log(C);
+
+var number = 1235429;
+var count = 0;
+var tempNum = number;
+
+// Handle the case when num is 0 separately
+if (tempNum === 0) {
+    count = 1;
+} else {
+    // Loop to count digits
+    while (tempNum >= 1) {
+        tempNum = tempNum / 10;
+        count++;
+    }
+}
+
+console.log(`Number of digits in ${number} is: ${count}`);
+
+
+
+
+
+
+
+
+const height = 5
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < height - i - 1; j++) {
+            process.stdout.write(" ");
+        }
+
+        for (let k = 0; k < 2 * i + 1; k++) {
+            process.stdout.write("*");
+        }
+
+        console.log();
+    }
+
+    for (let i = 0; i < height; i++) {
+        console.log("    |");
+    }
+
+
+
---
+++ start.js
@@ -0,0 +1,24 @@
+
+
+const firstName = 'Othmane';
+const lastName = 'stoutou';
+let PI = 3.14;
+let raduis = 201921;
+let favoriteSuperHero = "flashman";
+let favoriteQuote = "stay positive, work hard, make it happen";
+
+//1- My name :
+const fullName = firstName+" "+lastName;
+
+//2-Maths :
+const area = PI * raduis**2;
+const perimeter = 2*PI*raduis ;
+
+//3- Motivation :
+
+var motivation = "A wise man named "+favoriteSuperHero+" : "+favoriteQuote;
+
+console.log(fullName)
+console.log(area)
+console.log(perimeter)
+console.log(motivation)
